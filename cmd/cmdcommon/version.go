164ec4ef164636ceff94653a8175ddd07a1de8ee
Celrenheit <celrenheit+github@gmail.com>
2017-11-19 11:00:33 +0100

    slice commit

--- cmd/cmdcommon/version.go
+++ cmd/cmdcommon/version.go
@@ -5,5 +5,8 @@ import (
 )
 
 func DisplayVersion(app, version, commit, date string) string {
+	if len(commit) > 7 {
+		commit = commit[0:7]
+	}
 	return fmt.Sprintf("%v %v(%v) - %v", app, version, commit, date)
 }
