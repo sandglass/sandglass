99e724fb4db47bf77720f0df4f68407a80be040f
Celrenheit <celrenheit+github@gmail.com>
2017-11-19 19:50:08 +0100

    get rid of protobuf building

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-go run cmd/sandglass/main.go --name ${NAME} \
-        --discovery-backend etcd --discovery-addrs etcd:4001 \
-        --advertise-addr ${ADVERTISE_ADDR} --http-addr ${HTTP_ADDR} \
-        --grpc-addr ${GRPC_ADDR} \
-        --dbpath /tmp/${NAME}db \
-        --initial-peers ${INITIAL_PEERS}
