3761937354cf47f5f63e6d2ffda609d4228bd55d
Celrenheit <celrenheit+github@gmail.com>
2017-12-01 19:44:37 +0100

    read messages directly in the WAL now that the messages are available there

--- storage/storage.go
+++ storage/storage.go
@@ -17,7 +17,7 @@ type Storage interface {
 	LastKVForPrefix(prefix, suffix []byte) []byte
 	ForEach(fn func(msg *sgproto.Message) error) error
 	ForRange(min, max sandflake.ID, fn func(msg *sgproto.Message) error) error
-	ForEachKey(min []byte, fn func(k []byte) error) error
+	ForEachWALEntry(min []byte, fn func(msg *sgproto.Message) error) error
 }
 
 type Entry struct {
